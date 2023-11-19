aa2fb7219dd14e434a60d5105b01ab4167f0bfb1
daikitag <48062118+daikitag@users.noreply.github.com>
2023-11-19 10:46:11 +0000

    CODE: sim_trait
    Update the sim_trait function.

---
+++ tests/test_trait.py
@@ -0,0 +1,429 @@
+import msprime
+import numpy as np
+import pandas as pd
+import pytest
+import tstrait
+from scipy import stats
+from tstrait.base import _check_numeric_array
+from tstrait.trait import _TraitSimulator
+from tstrait.trait import sim_trait
+
+from .data import (
+    allele_freq_one,
+    binary_tree,
+    non_binary_tree,
+    simple_tree_seq,
+)  # noreorder
+
+
+@pytest.fixture(scope="class")
+def sample_ts():
+    ts = msprime.sim_ancestry(10, sequence_length=100_000, random_seed=1)
+    ts = msprime.sim_mutations(ts, rate=0.01, random_seed=1)
+    return ts
+
+
+@pytest.fixture(scope="class")
+def sample_trait_model():
+    return tstrait.trait_model(distribution="normal", mean=0, var=1)
+
+
+def freqdep(alpha, freq):
+    return np.sqrt(pow(2 * freq * (1 - freq), alpha))
+
+
+class TestInput:
+    """This test will check that an informative error is raised when the input parameter
+    does not have an appropriate type or value.
+    """
+
+    def test_input_type(self, sample_ts, sample_trait_model):
+        with pytest.raises(
+            TypeError, match="ts must be a <class 'tskit.trees.TreeSequence'> instance"
+        ):
+            sim_trait(ts=1, num_causal=1, model=sample_trait_model)
+        with pytest.raises(
+            TypeError,
+            match="model must be a <class 'tstrait.trait_model.TraitModel'> instance",
+        ):
+            sim_trait(ts=sample_ts, num_causal=1, model="model")
+
+    def test_bad_input(self, sample_ts, sample_trait_model):
+        ts = msprime.sim_ancestry(2, sequence_length=100_000, random_seed=1)
+        with pytest.raises(ValueError, match="No mutation in the tree sequence input"):
+            sim_trait(ts=ts, num_causal=1, model=sample_trait_model)
+        num_causal = sample_ts.num_sites + 1
+        with pytest.raises(
+            ValueError,
+            match="num_causal must be an integer not greater than the number of sites "
+            "in ts",
+        ):
+            sim_trait(ts=sample_ts, num_causal=num_causal, model=sample_trait_model)
+
+    @pytest.mark.parametrize("num_causal", [0, -1])
+    def test_num_causal_input(self, sample_ts, sample_trait_model, num_causal):
+        with pytest.raises(TypeError, match="num_causal must be an integer"):
+            sim_trait(ts=sample_ts, num_causal=1.0, model=sample_trait_model)
+        with pytest.raises(
+            ValueError, match="num_causal must be an integer not less than 1"
+        ):
+            sim_trait(ts=sample_ts, num_causal=num_causal, model=sample_trait_model)
+
+    def test_alpha_input(self, sample_ts, sample_trait_model):
+        with pytest.raises(TypeError, match="alpha must be numeric"):
+            sim_trait(ts=sample_ts, num_causal=1, model=sample_trait_model, alpha="1")
+
+
+class TestOutputDim:
+    """Check that the sim_trait function gives the correct output regardless of the
+    tree sequence mutation type or the trait model
+    """
+
+    def check_dimensions(self, df, num_causal):
+        assert len(df) == num_causal
+        assert df.shape[1] == 6
+        assert list(df.columns) == [
+            "position",
+            "site_id",
+            "effect_size",
+            "causal_state",
+            "allele_freq",
+            "trait_id",
+        ]
+        _check_numeric_array(df["site_id"], "site_id")
+        _check_numeric_array(df["effect_size"], "effect_size")
+        _check_numeric_array(df["trait_id"], "trait_id")
+        _check_numeric_array(df["position"], "position")
+        _check_numeric_array(df["allele_freq"], "allele_freq")
+
+    @pytest.mark.parametrize(
+        "trait_model",
+        [
+            tstrait.trait_model(distribution="normal", mean=0, var=1),
+            tstrait.trait_model(distribution="exponential", scale=1),
+            tstrait.trait_model(distribution="fixed", value=1),
+            tstrait.trait_model(distribution="t", mean=0, var=1, df=1),
+            tstrait.trait_model(distribution="gamma", shape=1, scale=1),
+        ],
+    )
+    def test_output(self, sample_ts, trait_model):
+        num_causal = 10
+        sim_result = sim_trait(ts=sample_ts, num_causal=num_causal, model=trait_model)
+        self.check_dimensions(sim_result, num_causal)
+        np.testing.assert_equal(sim_result["trait_id"], np.zeros(num_causal))
+        assert (
+            0 <= np.min(sim_result["site_id"])
+            and np.max(sim_result["site_id"]) < sample_ts.num_sites
+        )
+
+    def test_output_default_num_causal(self, sample_ts):
+        model = tstrait.trait_model(distribution="normal", mean=0, var=1)
+        sim_result = sim_trait(ts=sample_ts, model=model)
+        self.check_dimensions(sim_result, 1)
+
+        sim_result = sim_trait(ts=sample_ts, model=model, alpha=-1)
+        self.check_dimensions(sim_result, 1)
+
+    @pytest.mark.parametrize("alpha", [1, -1])
+    def test_output_alpha(self, alpha, sample_ts, sample_trait_model):
+        num_causal = 10
+        sim_result = sim_trait(
+            ts=sample_ts, num_causal=num_causal, model=sample_trait_model, alpha=alpha
+        )
+        self.check_dimensions(sim_result, num_causal)
+
+    def test_output_binary(self, sample_trait_model):
+        num_causal = 5
+        ts = msprime.sim_ancestry(10, sequence_length=100_000, random_seed=1)
+        ts = msprime.sim_mutations(ts, rate=0.01, random_seed=1, model="binary")
+        sim_result = sim_trait(ts=ts, num_causal=num_causal, model=sample_trait_model)
+        self.check_dimensions(sim_result, num_causal)
+
+    def test_output_multivariate(self, sample_ts):
+        mean = [0, 1]
+        cov = [[4, 1], [1, 4]]
+        num_causal = 5
+        model = tstrait.trait_model(distribution="multi_normal", mean=mean, cov=cov)
+        df = sim_trait(
+            ts=sample_ts,
+            num_causal=num_causal,
+            model=model,
+            random_seed=1,
+        )
+        trait_ids = df.trait_id.unique()
+        np.testing.assert_equal(trait_ids, np.array([0, 1]))
+        assert len(df) == num_causal * 2
+        df0 = df[df.trait_id == 0]
+        df1 = df[df.trait_id == 1]
+        self.check_dimensions(df0, num_causal)
+        self.check_dimensions(df1, num_causal)
+        assert (
+            0 <= np.min(df["site_id"]) and np.max(df["site_id"]) < sample_ts.num_sites
+        )
+        assert 0 <= np.min(df["trait_id"]) and np.max(df["trait_id"]) <= 1
+
+    def test_output_multivariate_alpha(self, sample_ts):
+        mean = [0, 1]
+        cov = [[4, 1], [1, 4]]
+        num_causal = 5
+        model = tstrait.trait_model(distribution="multi_normal", mean=mean, cov=cov)
+        df = sim_trait(
+            ts=sample_ts,
+            num_causal=num_causal,
+            model=model,
+            alpha=-1,
+            random_seed=1,
+        )
+        trait_ids = df.trait_id.unique()
+        np.testing.assert_equal(trait_ids, np.array([0, 1]))
+        assert len(df) == num_causal * 2
+        df0 = df[df.trait_id == 0]
+        df1 = df[df.trait_id == 1]
+        self.check_dimensions(df0, num_causal)
+        self.check_dimensions(df1, num_causal)
+        assert (
+            0 <= np.min(df["site_id"]) and np.max(df["site_id"]) < sample_ts.num_sites
+        )
+        assert 0 <= np.min(df["trait_id"]) and np.max(df["trait_id"]) <= 1
+
+
+def model_list():
+    loc = 2
+    scale = 5
+    df = 10
+    shape = 5
+    distr = [
+        (
+            tstrait.trait_model(distribution="normal", mean=loc, var=scale**2),
+            "norm",
+            (loc, scale),
+        ),
+        (
+            tstrait.trait_model(distribution="exponential", scale=scale),
+            "expon",
+            (0, scale),
+        ),
+        (
+            tstrait.trait_model(distribution="t", mean=loc, var=scale**2, df=df),
+            "t",
+            (df, loc, scale),
+        ),
+        (
+            tstrait.trait_model(distribution="gamma", shape=shape, scale=scale),
+            "gamma",
+            (shape, 0, scale),
+        ),
+    ]
+
+    return distr
+
+
+class Test_KSTest:
+    """
+    Test the distribution of effect sizes by using a Kolmogorov-Smirnov test.
+    rvs is array-like object of random variables, df is the output dataframe from
+    `tstrait.sim_trait`, dist is a name of a distribution in scipy.stats, and args
+    are the args for scipy.stats.dist(*args)
+    """
+
+    def check_distribution(self, rvs, dist, args=()):
+        D, pval = stats.kstest(rvs, dist, args=args, N=1000)
+        if pval < 0.05:
+            raise ValueError(f"KS test failed for distribution {dist}")
+
+    @pytest.mark.parametrize("model, distr, args", model_list())
+    def test_KStest(self, model, distr, args, sample_ts):
+        result = np.array([])
+        for i in range(2):
+            sim_result = sim_trait(
+                ts=sample_ts, num_causal=1000, model=model, random_seed=i
+            )
+            result = np.concatenate((result, sim_result["effect_size"]))
+        self.check_distribution(result, distr, args)
+
+    def test_fixed(self, sample_ts):
+        """
+        Some effect sizes are 0, as there are sites with only ancestral states
+        """
+        value = 4
+        model = tstrait.trait_model(distribution="fixed", value=value)
+        sim_result = sim_trait(ts=sample_ts, num_causal=1000, model=model)
+        data = sim_result.loc[sim_result.effect_size != 0]
+        np.testing.assert_allclose(data["effect_size"], np.ones(len(data)) * value)
+
+    def test_multivariate(self, sample_ts):
+        """
+        Conduct a KS test for individual effect sizes and arbitrary summation of
+        effect sizes.
+        """
+        np.random.seed(20)
+        n = 3
+        mean = np.random.randn(n)
+        M = np.random.randn(n, n)
+        cov = np.dot(M, M.T)
+        num_causal = 2000
+        model = tstrait.trait_model(distribution="multi_normal", mean=mean, cov=cov)
+        sim_result = sim_trait(
+            ts=sample_ts, num_causal=num_causal, model=model, random_seed=100
+        )
+        sim_result1 = sim_trait(
+            ts=sample_ts, num_causal=num_causal, model=model, random_seed=201
+        )
+        sim_result = pd.concat([sim_result, sim_result1])
+
+        sim_result = sim_result.reset_index()
+        del sim_result["index"]
+
+        const = np.random.randn(n)
+        data_val = np.matmul(const, cov)
+        data_sd = np.sqrt(np.matmul(data_val, const))
+        sum_data = np.zeros(2 * num_causal)
+
+        for i in range(n):
+            df = sim_result.loc[sim_result.trait_id == i]
+            self.check_distribution(
+                df["effect_size"],
+                "norm",
+                (mean[i], np.sqrt(cov[i, i])),
+            )
+            sum_data += df["effect_size"] * const[i]
+
+        self.check_distribution(
+            sum_data,
+            "norm",
+            (np.matmul(const, mean), data_sd),
+        )
+
+
+class TestGenotype:
+    """
+    Test the `_obtain_allele_count` method and check if can return the correct
+    results. Afterwards, we will check the `_sim_beta` method.
+    """
+
+    def test_binary_tree(self):
+        """
+        We will be testing the _TraitSimulator class against the
+        binary tree model.
+        """
+        ts = binary_tree()
+        alpha = -1
+        value = 2
+        fixed_model = tstrait.trait_model(distribution="fixed", value=value)
+        tree = ts.first()
+        simulator = _TraitSimulator(
+            ts, num_causal=ts.num_sites, model=fixed_model, alpha=alpha, random_seed=1
+        )
+
+        # We will examine `_obtain_allele_count` method against all possible states
+        # at a site to make sure that the code is running correctly
+        for state, num in zip(["A", "T", 1], [2, 2, 0]):
+            assert simulator._obtain_allele_count(tree, ts.site(0), state) == num
+        for state, num in zip(["A", "T", "C"], [2, 1, 1]):
+            assert simulator._obtain_allele_count(tree, ts.site(1), state) == num
+        for state, num in zip(["T", "G", "C", "A"], [0, 0, 1, 3]):
+            assert simulator._obtain_allele_count(tree, ts.site(2), state) == num
+        for state, num in zip(["T", "C", "A"], [2, 2, 0]):
+            assert simulator._obtain_allele_count(tree, ts.site(3), state) == num
+
+        result = simulator._sim_beta(np.arange(4), ["A", "A", "G", "C"])
+        np.testing.assert_array_almost_equal(
+            result.beta_array,
+            [
+                value * freqdep(alpha, 1 / 2),
+                value * freqdep(alpha, 1 / 2),
+                0,
+                value * freqdep(alpha, 1 / 2),
+            ],
+        )
+        np.testing.assert_array_almost_equal(
+            result.allele_freq, [1 / 2, 1 / 2, 0, 1 / 2]
+        )
+
+    def test_non_binary_tree(self):
+        """
+        Repeat the above procedure for non-binary tree.
+        """
+        ts = non_binary_tree()
+        value = 2
+        alpha = -1
+        fixed_model = tstrait.trait_model(distribution="fixed", value=value)
+        tree = ts.first()
+        simulator = _TraitSimulator(
+            ts, num_causal=ts.num_sites, model=fixed_model, alpha=alpha, random_seed=1
+        )
+
+        # We will examine `_obtain_allele_count` method against all possible states
+        # at a site to make sure that the code is running correctly
+        for state, num in zip(["A", "T"], [3, 3]):
+            assert simulator._obtain_allele_count(tree, ts.site(0), state) == num
+        for state, num in zip(["A", "T", "C"], [3, 1, 2]):
+            assert simulator._obtain_allele_count(tree, ts.site(1), state) == num
+
+        result = simulator._sim_beta([0, 1], ["A", "T"])
+        np.testing.assert_array_almost_equal(
+            result.beta_array,
+            [value * freqdep(alpha, 1 / 2), value * freqdep(alpha, 1 / 6)],
+        )
+        np.testing.assert_array_almost_equal(result.allele_freq, [1 / 2, 1 / 6])
+
+    def test_allele_freq_one(self):
+        """
+        Examine `sim_trait` function when allele frequency is 1.
+        """
+        ts = allele_freq_one()
+        value = 2
+        fixed_model = tstrait.trait_model(distribution="fixed", value=value)
+        trait_df = sim_trait(ts, num_causal=1, model=fixed_model, alpha=0)
+
+        np.array_equal(trait_df.site_id, [0])
+        np.array_equal(trait_df.effect_size, [value])
+        np.array_equal(trait_df.trait_id, [0])
+        np.array_equal(trait_df.allele_freq, [1])
+
+    def test_allele_freq_one_freq_dep(self):
+        """
+        Examine the frequency dependence model when allele frequency is 1 under
+        the frequency dependent model.
+        """
+        ts = allele_freq_one()
+        fixed_model = tstrait.trait_model(distribution="fixed", value=1)
+        trait_df = sim_trait(ts, num_causal=1, model=fixed_model, alpha=-1)
+
+        np.array_equal(trait_df.site_id, [0])
+        np.array_equal(trait_df.effect_size, [0])
+        np.array_equal(trait_df.trait_id, [0])
+        np.array_equal(trait_df.allele_freq, [1])
+
+
+class TestSimTrait:
+    def test_tree_seq(self):
+        """
+        We will be testing the `sim_trait` function against a simple
+        tree sequence data, where we only have 1 mutation on each site.
+        """
+        ts = simple_tree_seq()
+        value = 10
+        alpha = -1 / 2
+        fixed_model = tstrait.trait_model(distribution="fixed", value=value)
+        trait_df = sim_trait(
+            ts, num_causal=ts.num_sites, model=fixed_model, alpha=alpha
+        )
+        test_df = pd.DataFrame(
+            {
+                "position": ts.sites_position[np.arange(3)],
+                "site_id": np.arange(3),
+                "effect_size": [
+                    value * freqdep(alpha, 3 / 4),
+                    0,
+                    value * freqdep(alpha, 1 / 4),
+                ],
+                "causal_state": ["T", "A", "C"],
+                "allele_freq": [3 / 4, 1, 1 / 4],
+                "trait_id": np.zeros(3),
+            }
+        )
+
+        pd.testing.assert_frame_equal(trait_df, test_df, check_dtype=False)
+
+        assert trait_df.position.dtype == int
---
+++ tstrait/trait.py
@@ -0,0 +1,260 @@
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+import tskit
+import tstrait
+
+from .base import _check_instance
+from .base import _check_int
+from .base import _check_val
+
+
+@dataclass
+class _FreqResult:
+    """
+    Data class that contains simulated effect size and allele frequency.
+
+    Attributes
+    ----------
+    beta_array : numpy.array
+        Numpy array that includes simulated effect size.
+    allele_freq : numpy.array
+        Allele frequency of each causal mutation.
+    """
+
+    beta_array: np.array
+    allele_freq: np.array
+
+
+class _TraitSimulator:
+    """Simulator class to select causal alleles and simulate effect sizes of causal
+    mutations.
+
+    Parameters
+    ----------
+    ts : tskit.TreeSequence
+        Tree sequence data with mutation.
+    num_causal : int
+        Number of causal sites.
+    model : TraitModel
+        Trait model that will be used to simulate effect sizes.
+    alpha : float
+        Parameter that determines the degree of the frequency dependence model.
+    random_seed : int
+        The random seed.
+    """
+
+    def __init__(self, ts, num_causal, model, alpha, random_seed):
+        self.ts = ts
+        self.num_causal = num_causal
+        self.model = model
+        self.alpha = alpha
+        self.rng = np.random.default_rng(random_seed)
+
+    def _choose_causal_site(self):
+        """
+        Randomly chooses causal site IDs among all the sites in the tree sequence
+        data. The site IDs are aligned based on their genomic positions as a part of
+        the tree sequence data requirement, so the chosen site IDs are sorted in the
+        final step. The algorithm will be faster if the site IDs are aligned by
+        their genomic locations.
+        """
+        site_id = self.rng.choice(
+            self.ts.num_sites, size=self.num_causal, replace=False
+        )
+        return np.sort(site_id)
+
+    def _choose_causal_state(self, site_id):
+        """
+        Randomly chooses a causal state by choosing a causal mutation at random.
+        TODO: This code will include mutation that are not included in the sample node
+        (Otherwise we need to obtain the allele frequency). However, the probability of
+        choosing such mutation would be very low and a presence of a mutation in the tree
+        implies that the individual in some past generation had that mutation, so I think
+        it is fine for now. But we may need to modify it in the future.
+        """
+        causal_state = self.rng.choice(self.ts.site(site_id).mutations).derived_state
+
+        return causal_state
+
+    def _obtain_allele_count(self, tree, site, causal_state):
+        """Obtain a dictionary of allele counts, and the ancestral state is not
+        included in the dictionary. Input is the tree sequence site (`ts.site(ID)`)
+        instead of site ID, as obtaining `ts.site(ID)` can be time consuming. The
+        ancestral state is not deleted if the ancestral state is the only allele
+        at that site.
+        """
+        if site.ancestral_state == causal_state:
+            counts = self.ts.num_samples
+        else:
+            counts = 0
+        for m in site.mutations:
+            current_state = site.ancestral_state
+            if m.parent != tskit.NULL:
+                current_state = self.ts.mutation(m.parent).derived_state
+            # Silent mutations do nothing
+            if current_state != m.derived_state:  # pragma: no cover
+                if m.derived_state == causal_state:
+                    counts += tree.num_samples(m.node)
+                elif current_state == causal_state:
+                    counts -= tree.num_samples(m.node)
+
+        return counts
+
+    def _frequency_multiplier(self, allele_freq):
+        """
+        Calculates the frequency dependence constant [2p(1-p)]^alpha by using
+        the allele_freq input.
+        """
+        if allele_freq == 0 or allele_freq == 1:
+            const = 0
+        else:
+            const = np.sqrt(pow(2 * allele_freq * (1 - allele_freq), self.alpha))
+        return const
+
+    def _freq_dep(self, site_id_array, causal_state_array):
+        """
+        Obtains the frequency dependent constant [2p(1-p)]^alpha for each site ID.
+        """
+        tree = tskit.Tree(self.ts)
+        num_samples = self.ts.num_samples
+        freq_dep_array = []
+        allele_freq_array = []
+        for site_id, causal_state in zip(site_id_array, causal_state_array):
+            site = self.ts.site(site_id)
+            tree.seek(site.position)
+            freq = self._obtain_allele_count(tree, site, causal_state) / num_samples
+            freq_dep_array.append(self._frequency_multiplier(freq))
+            allele_freq_array.append(freq)
+
+        result = _FreqResult(beta_array=freq_dep_array, allele_freq=allele_freq_array)
+
+        return result
+
+    def _sim_beta(self, site_id_array, causal_state_array):
+        """
+        Simulates effect size by using the model given in the `model` input. If `alpha`
+        is non-zero, frequency dependence architecture is used.
+        """
+        beta_array_non_freq = self.model._sim_effect_size(
+            num_causal=self.num_causal, rng=self.rng
+        )
+
+        result = self._freq_dep(site_id_array, causal_state_array)
+        result.beta_array = np.multiply(
+            result.beta_array, np.transpose(beta_array_non_freq)
+        )
+
+        return result
+
+    def _run(self):
+        """
+        This method runs a simulation based on the input parameters. This method randomly
+        chooses causal sites and the corresponding causal state based on the `num_causal`
+        input. Afterwards, effect size of each causal site is simulated based on the
+        trait model given by the `model` input. If `alpha` is non-zero, frequency
+        dependence architecture is used.
+        """
+        site_id_array = self._choose_causal_site()
+        causal_state_array = []
+        for site_id in site_id_array:
+            causal_state_array.append(self._choose_causal_state(site_id))
+
+        result = self._sim_beta(site_id_array, causal_state_array)
+
+        num_trait = self.model.num_trait
+
+        position = self.ts.sites_position[site_id_array]
+        if np.array_equal(np.floor(position), position):  # pragma: no cover
+            position = position.astype(int)
+
+        trait_df = pd.DataFrame(
+            {
+                "position": np.repeat(position, num_trait),
+                "site_id": np.repeat(site_id_array, num_trait),
+                "effect_size": result.beta_array.flatten(order="F"),
+                "causal_state": np.repeat(causal_state_array, num_trait),
+                "allele_freq": np.repeat(result.allele_freq, num_trait),
+                "trait_id": np.tile(np.arange(num_trait), self.num_causal),
+            }
+        )
+
+        return trait_df
+
+
+def sim_trait(ts, model, *, num_causal=None, alpha=0, random_seed=None):
+    """
+    Randomly selects causal sites and the corresponding causal state, and simulates
+    effect sizes for each of the chosen causal site.
+
+    Parameters
+    ----------
+    ts : tskit.TreeSequence
+        The tree sequence data that will be used in the quantitative trait
+        simulation.
+    model : tstrait.TraitModel
+        Trait model that will be used to simulate effect sizes.
+    num_causal : int, default 1
+        Number of causal sites. If None, number of causal sites will be 1.
+    alpha : float, default 0
+        Parameter that determines the degree of the frequency dependence model. Please
+        see :ref:`frequency_dependence` for details on how this parameter influences
+        effect size simulation.
+    random_seed : int, default None
+        Random seed of simulation. If None, simulation will be conducted randomly.
+
+    Returns
+    -------
+    pandas.DataFrame
+        Trait dataframe that includes simulated effect sizes.
+
+    Raises
+    ------
+    ValueError
+        If the number of mutations in `ts` is smaller than `num_causal`.
+
+    See Also
+    --------
+    trait_model : Return a trait model, which can be used as `model` input.
+    genetic_value : The trait dataframe output can be used as an input to obtain
+        genetic values.
+
+    Notes
+    -----
+    The simulation output is given as a :py:class:`pandas.DataFrame` and contains the
+    following columns:
+
+        * **site_id**: Site IDs that have causal mutation.
+        * **effect_size**: Simulated effect size of causal mutation.
+        * **causal_state**: Derived state of causal mutation.
+        * **allele_freq**: Allele frequency of causal state.
+        * **trait_id**: Trait ID.
+
+    Examples
+    --------
+    See :ref:`effect_size_sim` for worked examples.
+    """
+    ts = _check_instance(ts, "ts", tskit.TreeSequence)
+    model = _check_instance(model, "model", tstrait.TraitModel)
+    num_causal = 1 if num_causal is None else num_causal
+    num_causal = _check_int(num_causal, "num_causal", minimum=1)
+    alpha = _check_val(alpha, "alpha")
+    num_sites = ts.num_sites
+    if num_sites == 0:
+        raise ValueError("No mutation in the tree sequence input")
+    if num_causal > num_sites:
+        raise ValueError(
+            "num_causal must be an integer not greater than the number of sites in ts"
+        )
+
+    simulator = _TraitSimulator(
+        ts=ts,
+        num_causal=num_causal,
+        model=model,
+        alpha=alpha,
+        random_seed=random_seed,
+    )
+    trait_df = simulator._run()
+
+    return trait_df
